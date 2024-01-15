5b15f65782a9f9516e4e50e52e8cdc61e427fe0e
Brigitte Boerland <brigitteboerland@hotmail.com>
2024-01-15 23:40:03 +0100

    States exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import './App.css';
 import { portfolioItems } from './utils/data';
+import { PortfolioItemPage } from './pages/PortfolioItemPage';
+import { PortfolioPage } from './pages/PortfolioPage';
+
 
 export const App = () => {
-  console.log(portfolioItems); // Check console to see how portfolioItems look like. You can delete this after.
-  return <div className="App"></div>;
+  const [selectedItem, setSelectedItem] = useState(portfolioItems[1]);
+
+
+  return <div className="App">
+    {selectedItem ?
+  <PortfolioItemPage item={selectedItem}/> :
+  <PortfolioPage />
+}
+  </div>;
 };
---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,7 @@
+export const PortfolioPage = () => {
+    return (
+        <>
+        <p>portfolio page placeholder text</p>
+        </>
+    )
+}
